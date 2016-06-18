f9f5d5c62afe563f4243709986231635e10ba240
Zhang Hai <dreaming.in.code.zh@gmail.com>
2016-06-18 09:38:38 +0800

    Add override annotation.

--- src/me/zhanghai/mipsasm/assembler/Immediate.java
+++ src/me/zhanghai/mipsasm/assembler/Immediate.java
@@ -26,6 +26,7 @@ public class Immediate implements Operand, AssemblyProvider {
         return value;
     }
 
+    @Override
     public BitArray assemble(AssemblyContext context) {
         return value;
     }
